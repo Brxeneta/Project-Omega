fc05bbccb3a8b7b8c52ad37aabfb7a120db9f51d
Brxeneta <45110797+Brxeneta@users.noreply.github.com>
2019-04-25 20:08:26 -0400

    Create Quest.cs

---
+++ Quest.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Quest
+{
+    public bool isActive;
+
+    public string title;
+    public string description;
+    public int expReward;
+    public int goldReward;
+
+    public questGoal goal;
+
+    public void Complete()
+    {
+        isActive = false;
+    }
+}
